8c56f969f1444a86fb814995116a7119eb9f199a
Chisanupong Srisaket <srisaket246@gmail.com>
2026-02-08 09:31:49 +0700

    fix

--- server/db.ts
+++ server/db.ts
@@ -9,7 +9,7 @@ export const db = mysql.createPool({
   port: Number(process.env.DB_PORT || 3306),
 
   ssl: {
-  rejectUnauthorized: false
+  ca: process.env.DB_SSL_CA
 },
 
   waitForConnections: true,
